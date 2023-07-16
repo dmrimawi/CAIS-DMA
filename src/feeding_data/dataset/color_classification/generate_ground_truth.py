6510fb098454dd4c012addc94b96606eccd58014
Diaeddin Rimawi <drimawi@unibz.it>
2023-07-16 08:00:51 +0200

    Update colored cubes dataset generation script

--- src/feeding_data/dataset/color_classification/generate_ground_truth.py
+++ src/feeding_data/dataset/color_classification/generate_ground_truth.py
@@ -9,12 +9,13 @@ csvfile = open('objects_colors_dataset.csv', 'a', newline='')
 writer = csv.writer(csvfile)
 writer.writerow(header)
 
-files = os.listdir("color_classification")
+files = os.listdir(os.path.join("."))
 
 for f in files:
     if "," in f:
         print("FAILIER AT FILE: {}".format(f))
         break
-    color = [x for x in colors if x in f]
-    l = [f, color[0], "Cube", colors.index(color[0]) + 1]
-    writer.writerow(l)
+    if f.endswith(".jpg"):
+        color = [x for x in colors if x in f]
+        l = [f, color[0], "Cube", colors.index(color[0]) + 1]
+        writer.writerow(l)
